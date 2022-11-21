d6ae8fc84fbe0981e3ff32bc1d81ca7c3da37a58
THANG NGUYEN <thangnguyen24111990@gmail.com>
2022-11-21 22:08:28 +0200

    Update font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,24 @@ import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import NextNProgress from 'nextjs-progressbar';
-import '../styles/globals.scss';
+import '../styles/global.scss';
 import '../styles/slick.scss';
 import { ToastContainer } from 'react-toastify';
 import Layout from '@components/common/Layout';
+import localFont from '@next/font/local';
+
+const josh = localFont({
+  src: [
+    {
+      path: '../styles/fonts/Jost.woff2',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/Jost-Italic.woff2',
+      style: 'italic',
+    },
+  ],
+});
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(
@@ -27,7 +41,9 @@ function MyApp({ Component, pageProps }) {
       <Hydrate state={pageProps.dehydratedState}>
         <Layout>
           <NextNProgress color="#29D" startPosition={0.3} height={5} showOnShallow={false} />
-          <Component {...pageProps} />
+          <main className={josh.className}>
+            <Component {...pageProps} />
+          </main>
           <ToastContainer
             position="bottom-right"
             hideProgressBar={true}
